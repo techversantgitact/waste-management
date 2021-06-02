9bfaa2a7578cd5e5490027dab3fadfb0df12f1df
aswathibabu <85116832+aswathibabu@users.noreply.github.com>
2021-06-02 19:53:19 +0530

    Add files via upload

--- wasteweb/wasteapp/views.py
+++ wasteweb/wasteapp/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render,redirect
-#from django.http import HttpResponse
-
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
 
 def index(request):
     context = {}
@@ -18,12 +19,20 @@ def contact(request):
     context = {}
     return render(request, 'wasteapp/contact.html', context)
 
-def login(request):
+def signin(request):
     context = {}
     return render(request, 'wasteapp/signin.html', context)
 
 def signup(request):
-    context = {}
+    form = UserCreationForm()
+    
+    if request.method == "post":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('signin')
+            
+    context = {'form':form}
     return render(request, 'wasteapp/signup.html',context)
     
 def logout(request):
