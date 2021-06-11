fa425d4dde3690c9dec5ba5ca9b62ea3e480eccb
aswathibabu <85116832+aswathibabu@users.noreply.github.com>
2021-06-11 12:22:31 +0530

    Add files via upload

--- wasteweb/wasteapp/booking_functions/availability.py
+++ wasteweb/wasteapp/booking_functions/availability.py
@@ -1,13 +1,13 @@
 import datetime
 from wasteapp.models import Schedule,Booking,Truck
 
-'''def check_availability(schedule,booking_time):
+def check_availability(schedule,booking_time,pickup_time):
     avail_list = []
     booking_list = Booking.objects.filter(schedule=schedule)
     for booking in booking_list:
-        if booking.booking_time == schedule :
+        if booking.booking_time > pickup_time or booking.pickup_time < booking_time:
             avail_list.append(True) 
         else:
             avail_list.append(False)
-    return all(avail_list)       ''' 
+    return all(avail_list)      
     
